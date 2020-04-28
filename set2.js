ef22cbd7a300caf9b3d791bd984a41985aae0b25
ssheaks <sarah.sheaks@gmail.com>
2020-04-28 16:59:34 -0700

    jediName function

--- set2.js
+++ set2.js
@@ -6,3 +6,9 @@
 // firstName - a person's first name
 // lastName - a person's last name
 // The function should return the person's Jedi name. A Jedi name is formed from the first three letters of the last name, and the first two letters of the first name. For example, jediName('Beyonce', 'Knowles') should return 'KnoBe'.
+function jediName(firstName, lastName) {
+    const name = `${lastName.slice(0, 3)}${firstName.slice(0,2)}`;
+    return name;
+}
+const myName = jediName('Sarah', 'Borris');
+console.log(myName);
