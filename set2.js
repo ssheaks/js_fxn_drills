6441899d8606ca150896b8b3a256a1e4902db301
ssheaks <sarah.sheaks@gmail.com>
2020-04-28 16:53:47 -0700

    added set2.js

---
+++ set2.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Jedi name
+// Write a function called jediName which takes two arguments:
+
+// firstName - a person's first name
+// lastName - a person's last name
+// The function should return the person's Jedi name. A Jedi name is formed from the first three letters of the last name, and the first two letters of the first name. For example, jediName('Beyonce', 'Knowles') should return 'KnoBe'.
