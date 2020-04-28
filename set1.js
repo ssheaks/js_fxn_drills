fffde346ac963c22c841e48270a79d233d6d958a
ssheaks <sarah.sheaks@gmail.com>
2020-04-28 14:15:55 -0700

    add createGreeting fxn

---
+++ set1.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+/* eslint-disable quotes */
+'use strict';
+
+function createGreeting() {
+    console.log(`Hi, my name is Sarah and I'm 1,000 years old`);
+}
+
+createGreeting();
