08fb38631a73f2ab0176ea816a396929c64e482b
ssheaks <sarah.sheaks@gmail.com>
2020-04-28 14:24:37 -0700

    add year of birth fxn

--- set1.js
+++ set1.js
@@ -2,9 +2,16 @@
 /* eslint-disable quotes */
 'use strict';
 
+function getYearOfBirth(age) {
+    const yearOfBirth = 2020 - age;
+    return yearOfBirth
+}
+
 function createGreeting(name, age) {
     return `Hi, my name is ${name} and I'm ${age} years old`;
 }
 
+const yob1 = getYearOfBirth(1000);
+console.log(yob1);
 const greeting1 = createGreeting('Sarah', 1000);
 console.log(greeting1);
